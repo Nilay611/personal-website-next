53a777a7e7b99ea46931b99276f480dc418954c2
Aether61198 <nilaysaxena98@gmail.com>
2025-02-04 02:57:53 +0530

    Added button click handle

--- src/app/components/ui/MagicButton.tsx
+++ src/app/components/ui/MagicButton.tsx
@@ -17,6 +17,7 @@ const MagicButton = ({
     <button
       className={`font-jetbrainsMono inline-flex gap-3 h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-7 font-medium text-slate-400
       hover:translate-x-[-3px] hover:translate-y-[3px] transition-all ${otherClasses}`}
+      onClick={handleClick}
     >
       {icon && position === "left" && icon}
       {text}
